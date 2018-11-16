4be35bfe4c9514bd6b250c81f7ff07762bfa7c76
fehersanyi-bitrise <sandor.feher@bitrise.io>
2018-11-16 10:56:18 +0100

    drawing is done, need refactoring

--- pong.go
+++ pong.go
@@ -67,7 +67,30 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not load background %v", err)
 	}
+
+	//rendering players
+	hitBox1 := &sdl.Rect{X: 5, Y: 240, W: 32, H: 120}
+	hitBox2 := &sdl.Rect{X: 800 - 37, Y: 240, W: 32, H: 120}
+	ballBox := &sdl.Rect{X: 400 - 8, Y: 300 - 8, W: 16, H: 16}
+
+	player1, err := img.LoadTexture(renderer, "resources/images/player.png")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not load player1 image %v", err)
+	}
+	player2, err := img.LoadTexture(renderer, "resources/images/player2.png")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not load player2 image %v", err)
+	}
+	ball, err := img.LoadTexture(renderer, "resources/images/ball.png")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not load ball image %v", err)
+	}
+
 	renderer.Copy(texture, nil, nil)
+	renderer.Copy(player1, nil, hitBox1)
+	renderer.Copy(player2, nil, hitBox2)
+	renderer.Copy(ball, nil, ballBox)
 	renderer.Present()
+
 	time.Sleep(time.Second * 5)
 }
